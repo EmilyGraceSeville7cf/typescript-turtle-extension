185d34fd8f30316275bd6f83676b5d8ae4e71440
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-07-22 04:49:18 +1000

    fix: don't suggest words if they are the part of other completion

--- src/words.ts
+++ src/words.ts
@@ -3,6 +3,7 @@ import * as utils from "./utils"
 import * as commands from "./commands"
 import * as keywords from "./keywords"
 import * as variables from "./variables"
+import * as userDefinedIdentifiers from "./userDefinedIdentifiers"
 
 function __createCompletion(word: string): vscode.CompletionItem {
     const completion = new vscode.CompletionItem(word, vscode.CompletionItemKind.Text);
@@ -19,6 +20,7 @@ export function listFor(document: vscode.TextDocument): string[] {
     return [...new Set(document.getText().split(/\W/))].filter(word =>
         commands.list.map(command => command.name).indexOf(word) === -1 &&
         keywords.list.map(keyword => keyword.name).indexOf(word) === -1 &&
-        variables.list.map(variable => variable.name).indexOf(word) === -1
+        variables.list.map(variable => variable.name).indexOf(word) === -1 &&
+        userDefinedIdentifiers.listFor(document).indexOf(word) === -1
     )
 }
