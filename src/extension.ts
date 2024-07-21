182a7d377ba317a9db99857ac94702d39d92067f
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-07-22 01:10:13 +1000

    feat: move diagnostics to a separate file

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as keywords from './keywords';
 import * as words from './words';
 import * as constants from './constants';
 import * as userDefinedIdentifiers from './userDefinedIdentifiers';
-import * as builtinIdentifiers from './builtinIdentifiers';
+import * as patternDiagnostics from './patternDiagnostics';
 
 export function activate(context: vscode.ExtensionContext) {
     const provider = vscode.languages.registerCompletionItemProvider('scheme', {
@@ -27,51 +27,6 @@ export function activate(context: vscode.ExtensionContext) {
     subscribeToDocumentChanges(context, diagnostics);
 }
 
-
-interface PatternDiagnostic {
-    readonly regex: RegExp;
-    readonly description: string;
-    readonly severity: vscode.DiagnosticSeverity;
-}
-
-function newPatternDiagnostic(regex: RegExp, description: string, severity: vscode.DiagnosticSeverity): PatternDiagnostic {
-    return { regex, description, severity }
-}
-
-const allCommandNames = commands.list.map(command => command.name).concat(
-    keywords.list.map(keyword => keyword.name)
-)
-
-const patternDiagnostics = [
-    newPatternDiagnostic(/\(\s*\d+(\s+\d+)?\s*\)/, "Red, green, blue color components were expected, less were found.", vscode.DiagnosticSeverity.Warning),
-    newPatternDiagnostic(/\(\s*\d+(\s+\d+){3,}\s*\)/, "Just red, green, blue color components were expected, more were found.", vscode.DiagnosticSeverity.Warning),
-    newPatternDiagnostic(/\([^()a-zA-Z]*-\d+[^()a-zA-Z]*\)/, "Just positive color components expected, negative were found.", vscode.DiagnosticSeverity.Warning),
-    newPatternDiagnostic(/\(\s*\)/, "Valid command expected.", vscode.DiagnosticSeverity.Warning),
-    newPatternDiagnostic(/\(\s*define\s+turtle-(configuration|theme)\s+\(/, "It's recommended to escape the whole command list with a single quote.", vscode.DiagnosticSeverity.Information),
-    newPatternDiagnostic(/\(\s+\S/, "It's recommended to remove spaces right after the opening parenthesis.", vscode.DiagnosticSeverity.Information),
-    newPatternDiagnostic(/\S\s+\)/, "It's recommended to remove spaces right before the opening parenthesis.", vscode.DiagnosticSeverity.Information),
-].concat(
-    commands.list.filter(command => command.args !== undefined).map(command => {
-        let regex = `\\(\\s*${command.name}((\\s+-?\\d+){0,${command.args!.length - 1}}|(\\s+-?\\d+){${command.args!.length + 1},})\\s*\\)`
-
-        const diagnostic = newPatternDiagnostic(
-            new RegExp(regex),
-            `'${command.name}' expected exactly ${command.args?.length} arguments.`,
-            vscode.DiagnosticSeverity.Error
-        )
-        return diagnostic
-    }
-    )
-).concat(
-    commands.list.filter(command => command.args !== undefined).map(command =>
-        newPatternDiagnostic(
-            new RegExp(`\\(\\s*${command.name}(\\s+-?\\d+)*(\\s+[a-zA-Z]+)(\\s+-?\\d+)*\\)`),
-            `'${command.name}' expected integer arguments`,
-            vscode.DiagnosticSeverity.Error
-        )
-    )
-)
-
 export function refreshDiagnostics(document: vscode.TextDocument, targetDiagnostics: vscode.DiagnosticCollection): void {
     if (document.languageId !== "scheme")
         return
@@ -80,32 +35,10 @@ export function refreshDiagnostics(document: vscode.TextDocument, targetDiagnost
 
     for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
         const line = document.lineAt(lineIndex);
-        tryCreateDiagnostic(document, line, lineIndex, diagnostics);
+        patternDiagnostics.tryCreateDiagnosticFor(document, line, lineIndex, diagnostics);
     }
 
     targetDiagnostics.set(document.uri, diagnostics);
-}
-
-function tryCreateDiagnostic(document: vscode.TextDocument, line: vscode.TextLine, lineIndex: number, diagnostics: vscode.Diagnostic[]) {
-    const alternatives = allCommandNames.concat(builtinIdentifiers.list)
-        .concat(userDefinedIdentifiers.listFor(document)).join("|")
-
-    const patternDiagnostic = patternDiagnostics.concat([
-        newPatternDiagnostic(new RegExp(`\\(\\s*(?!(${alternatives})(\\s*\\)|\\s+[^()]*))[^()]*\\)`),
-            "Unknown command or identifier, possibly built-in or defined externally.",
-            vscode.DiagnosticSeverity.Warning)
-    ]).find(patternDiagnostic => patternDiagnostic.regex.test(line.text))
-    if (patternDiagnostic === undefined)
-        return
-
-    const index = line.text.search(patternDiagnostic.regex);
-    const match = line.text.match(patternDiagnostic.regex)![0]
-    const range = new vscode.Range(lineIndex, index, lineIndex, index + match.length);
-
-    const diagnostic = new vscode.Diagnostic(range, patternDiagnostic.description,
-        patternDiagnostic.severity);
-    diagnostic.code = "turtle";
-    diagnostics.push(diagnostic);
 }
 
 export function subscribeToDocumentChanges(context: vscode.ExtensionContext, diagnostics: vscode.DiagnosticCollection): void {
---
+++ src/patternDiagnostics.ts
@@ -0,0 +1,94 @@
+import * as vscode from 'vscode';
+import * as builtinIdentifiers from './builtinIdentifiers';
+import * as commands from './commands'
+import * as keywords from './keywords';
+import * as userDefinedIdentifiers from './userDefinedIdentifiers';
+import * as variables from './keywords';
+
+export interface PatternDiagnostic {
+    readonly regex: RegExp;
+    readonly description: string;
+    readonly severity: vscode.DiagnosticSeverity;
+}
+
+function __create(regex: RegExp, description: string, severity: vscode.DiagnosticSeverity): PatternDiagnostic {
+    return { regex, description, severity }
+}
+
+const __stylePatternDiagnostics = [
+    __create(/\(\s*define\s+turtle-(configuration|theme)\s+\(/, "It's recommended to escape the whole command list with a single quote.", vscode.DiagnosticSeverity.Information),
+    __create(/\(\s+\S/, "It's recommended to remove spaces right after the opening parenthesis.", vscode.DiagnosticSeverity.Information),
+    __create(/\S\s+\)/, "It's recommended to remove spaces right before the opening parenthesis.", vscode.DiagnosticSeverity.Information),
+]
+
+const __contextUnawarePatternDiagnostics = [
+    __create(/\(\s*\d+(\s+\d+)?\s*\)/, "Red, green, blue color components were expected, less were found.", vscode.DiagnosticSeverity.Warning),
+    __create(/\(\s*\d+(\s+\d+){3,}\s*\)/, "Just red, green, blue color components were expected, more were found.", vscode.DiagnosticSeverity.Warning),
+    __create(/\([^()a-zA-Z]*-\d+[^()a-zA-Z]*\)/, "Just positive color components expected, negative were found.", vscode.DiagnosticSeverity.Warning),
+    __create(/\(\s*\)/, "Valid command expected.", vscode.DiagnosticSeverity.Warning),
+]
+
+const __argumentCountPatternDiagnostics = commands.list
+    .filter(command => command.args !== undefined)
+    .map(command => {
+        let regex = `\\(\\s*${command.name}((\\s+-?\\d+){0,${command.args!.length - 1}}|(\\s+-?\\d+){${command.args!.length + 1},})\\s*\\)`
+
+        const diagnostic = __create(
+            new RegExp(regex),
+            `'${command.name}' expected exactly ${command.args?.length} arguments.`,
+            vscode.DiagnosticSeverity.Error
+        )
+        return diagnostic
+    })
+
+const __argumentTypePatternDiagnostics = commands.list
+    .filter(command => command.args !== undefined)
+    .map(command =>
+        __create(
+            new RegExp(`\\(\\s*${command.name}(\\s+-?\\d+)*(\\s+[a-zA-Z]+)(\\s+-?\\d+)*\\)`),
+            `'${command.name}' expected integer arguments`,
+            vscode.DiagnosticSeverity.Error
+        )
+    )
+
+export function tryCreateDiagnosticFor(document: vscode.TextDocument, line: vscode.TextLine, lineIndex: number, diagnostics: vscode.Diagnostic[]) {
+    const alternatives = builtinIdentifiers.list.concat(
+        commands.list.map(command => command.name)
+    ).concat(
+        keywords.list.map(keyword => keyword.name)
+    ).concat(
+        userDefinedIdentifiers.createUserDefinedIdentifierCompletionsFor(document)
+            .filter(userDefinedIdentifier =>
+                [
+                    vscode.CompletionItemKind.Variable,
+                    vscode.CompletionItemKind.Function
+                ].indexOf(userDefinedIdentifier.kind!) !== -1).map(userDefinedIdentifier => userDefinedIdentifier.label as string)
+    ).concat(
+        variables.list.map(variable => variable.name)
+    ).join("|")
+
+    const allPatternDiagnostics = __stylePatternDiagnostics
+        .concat(__contextUnawarePatternDiagnostics)
+        .concat(__argumentCountPatternDiagnostics)
+        .concat(__argumentTypePatternDiagnostics)
+        .concat(__argumentTypePatternDiagnostics)
+
+    const patternDiagnostic = allPatternDiagnostics.concat(
+        __create(new RegExp(`\\(\\s*(?!(${alternatives})(\\s*\\)|\\s+[^()]*))[^()]*\\)`),
+            "Unknown command or identifier, possibly unknown built-in or defined externally.",
+            vscode.DiagnosticSeverity.Warning)
+    ).find(patternDiagnostic => patternDiagnostic.regex.test(line.text))
+
+    if (patternDiagnostic === undefined)
+        return
+
+    const index = line.text.search(patternDiagnostic.regex);
+    const match = line.text.match(patternDiagnostic.regex)![0]
+    const range = new vscode.Range(lineIndex, index, lineIndex, index + match.length);
+
+    const diagnostic = new vscode.Diagnostic(range, patternDiagnostic.description,
+        patternDiagnostic.severity);
+
+    diagnostic.code = "turtle";
+    diagnostics.push(diagnostic);
+}
